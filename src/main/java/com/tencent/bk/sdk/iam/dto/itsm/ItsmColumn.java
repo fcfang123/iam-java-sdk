dc94c95d3a6608d325945b234f127bc744eec638
greysonfang <greysonfang@tencent.com>
2023-09-01 15:26:55 +0800

    feat：增加itsm审批单据内容信息 #64

--- src/main/java/com/tencent/bk/sdk/iam/dto/itsm/ItsmColumn.java
+++ src/main/java/com/tencent/bk/sdk/iam/dto/itsm/ItsmColumn.java
@@ -1,13 +1,28 @@
 package com.tencent.bk.sdk.iam.dto.itsm;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class ItsmColumn {
+    /**
+     * 列标识
+     * */
     private String key;
+    /**
+    * 列名称
+    * */
     private String name;
+    /**
+     * 列类型：text、url
+     * */
     private String type;
+    /**
+     * 是否为抽屉模式
+     * */
+    @JsonProperty("is_iframe")
+    private Boolean iframe;
     private Integer width;
 }
