e44b2bf26f4d60be06506ccacaceb05409f34775
greysonfang <greysonfang@tencent.com>
2024-01-25 21:01:33 +0800

    feat：增加人员模板操作相关接口 #89

--- src/main/java/com/tencent/bk/sdk/iam/dto/manager/vo/SubjectTemplateVO.java
+++ src/main/java/com/tencent/bk/sdk/iam/dto/manager/vo/SubjectTemplateVO.java
@@ -1,9 +1,11 @@
 package com.tencent.bk.sdk.iam.dto.manager.vo;
 
 import com.tencent.bk.sdk.iam.dto.manager.SubjectTemplateInfo;
+import lombok.Data;
 
 import java.util.List;
 
+@Data
 public class SubjectTemplateVO {
     int count;
     List<SubjectTemplateInfo> results;
